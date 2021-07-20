4b1ddf177aa10bf05907fa68c10e696206549993
unknown <e.mosychenko@ukr.net>
2021-07-20 15:23:12 +0300

    first commit

---
+++ js/data.js
@@ -0,0 +1,35 @@
+const data = {
+    "first_name": "Evgen",
+    "last_name": "Mosychenko",
+    "job_title": "MARKETING DIRECTOR",
+    "photo": "./images/photo_2021-02-07_23-39-53.jpg",
+    "phone": "+380976010846",
+    "email": "dosofi.ua@gmail.com",
+    "skype": "skype_number",
+    "linkedin": "https://www.linkedin.com/in/evgen-mosychenko-98b816202/",
+    "address": "221B Baker Street, London",
+    "skills": [
+        "Creative Development",
+        "Branding & Identity",
+        "Project Management",
+        "Contact Negotiation",
+        "Ad Buying & Planning",
+        "Product Positioning",
+        "Visual Merchandising",
+        "Search Engine Ads",
+        "Process Improvement",
+        "Market Strategizing",
+        "Adobe Creative Suite",
+        "Microsoft Office 2016",
+        "Google Web Analytics",
+        "Fluent in Spanish",
+    ],
+    "education": [
+        ["2008-2010", "University of Alabama English Language Institute"],
+        ["2010-2015", "University of Alabama English Language Institute"],
+    ],
+    "achievements": [
+        "Marketer of the Year Academic Scholorship Deans List: 2006-2007, National Merit Scholarship Corporation",
+    ],
+    "profile": "This paragraph will be you first impressions to recruiters. An effective summary is short, concise and focuses on your most important selling points. You should align your summary with the job requirements by highlighting the key strengths and skills that will truly make you stand out as a highly qualified candidate. Transforming responsibilities into quantifiable achievements with numbers communicates your value."
+};
